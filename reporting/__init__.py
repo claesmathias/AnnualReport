977ea7dd8fe0daded636ef748b9c83091430806b
Mathias Claes <claesmathias@gmail.com>
2015-02-11 21:30:29 +0100

    added ProfitabilityIndicators

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -4,8 +4,10 @@ __author__ = 'claesma1'
 import os, glob
 from XMLParser import XMLParser
 from AnnualReport import AnnualReport
-from BarChart import BarChart
+from OperationsBarChart import OperationsBarChart
 from PieChart import PieChart
+from OperatingPerformanceBarChart import OperatingPerformanceBarChart
+from ProfitabilityIndicators import ProfitabilityIndicators
 
 
 def main():
@@ -13,7 +15,7 @@ def main():
     list = []
 
     # Find all the annual reports in the current directory
-    for file in glob.glob( "annual_report_*.xml" ):
+    for file in glob.glob("annual_report_*.xml"):
         # Parse the first file found
         XMLParser(file).parse(list)
 
@@ -21,10 +23,16 @@ def main():
     # list = sorted(list, key=lambda item: item.Info['Year'])
 
     # Create new bar chart
-    BarChart(list).create()
+    OperationsBarChart(list).create()
 
     # Create new pie char
     PieChart(list).create()
+
+    # Create new horizontal bar char
+    OperatingPerformanceBarChart(list).create()
+
+    # Create
+    ProfitabilityIndicators(list).create()
 
 
 if __name__ == "__main__":
