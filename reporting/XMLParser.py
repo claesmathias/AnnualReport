f25690213d3d29c2f76bb8976f200209622bbab8
Mathias Claes <claesmathias@gmail.com>
2015-02-16 12:09:25 +0100

    add QuarterlyEarnings and filter options

--- reporting/XMLParser.py
+++ reporting/XMLParser.py
@@ -2,14 +2,14 @@ __author__ = 'claesmathias'
 
 import xml.etree.ElementTree as ET
 from Utilities import Utilities
-from AnnualReport import AnnualReport
+from Reporting import *
 
 class XMLParser():
 
     def __init__(self, file):
         self.file = file
 
-    def parse(self, list):
+    def parse(self, list, function_filter):
         # Parse the given file
         tree = ET.parse(self.file)
 
@@ -18,19 +18,26 @@ class XMLParser():
 
         # Find the 'Report' tag
         for report in root.findall('Report'):
-            # Create a new annual report
-            ar = AnnualReport()
+            # Find the info element
+            info = report.find('Info')
+
+            # Check the type
+            if info.find('Type').text == "10-K":
+                # Create a new annual report
+                r = AnnualReport()
+            elif info.find('Type').text == "10-Q":
+                # Create a new quarterly earnings
+                r = QuarterlyEarnings()
 
             #
             # Parse the info statement, Iterate over keys and set values
-            info = report.find('Info')
-            i = ar.Info
+            i = r.Info
             Utilities.set_dictionary_xml(i, info)
 
             #
             # Parse the Consolidated Statements of Operations Data
             operations = report.find('Operations')
-            o = ar.Operations
+            o = r.Operations
             o.Revenue = operations.find('Revenue').text
             o.CostGoodsSold = operations.find('CostGoodsSold').text
             o.ProvisionIncomeTaxes = operations.find('ProvisionIncomeTaxes').text
@@ -38,41 +45,45 @@ class XMLParser():
 
             # Parse the operating costs
             operating_cost = operations.find('OperatingCosts')
-            oc = ar.Operations.OperatingCosts
+            oc = r.Operations.OperatingCosts
             # Iterate over keys and set values
             Utilities.set_dictionary_xml(oc, operating_cost)
 
             # Parse the operating income
             operating_income = operations.find('OperatingIncome')
-            oi = ar.Operations.OperatingIncome
+            oi = r.Operations.OperatingIncome
             # Iterate over keys and set values
             Utilities.set_dictionary_xml(oi, operating_income)
 
-            #
-            # Parse the Consolidated Balance Sheets
-            balance = report.find('Balance')
+            # For now only parse the Balance sheet for annual reports
+            if isinstance(r, AnnualReport):
+                #
+                # Parse the Consolidated Balance Sheets
+                balance = report.find('Balance')
 
-            # Parse the assets
-            assets = balance.find('Assets')
-            a = ar.Balance.Assets
-            a.Total = assets.find('Total').text
-            # Parse the current assets
-            current_assets = assets.find('CurrentAssets')
-            ca = a.CurrentAssets
-            # Iterate over keys and set values
-            Utilities.set_dictionary_xml(ca, current_assets)
+                # Parse the assets
+                assets = balance.find('Assets')
+                a = r.Balance.Assets
+                a.Total = assets.find('Total').text
+                # Parse the current assets
+                current_assets = assets.find('CurrentAssets')
+                ca = a.CurrentAssets
+                # Iterate over keys and set values
+                Utilities.set_dictionary_xml(ca, current_assets)
 
-            # Parse the liabilities
-            liabilities = balance.find('Liabilities')
-            l = ar.Balance.Liabilities
-            # Parse the current assets
-            current_liabilities = liabilities.find('CurrentLiabilities')
-            cl = l.CurrentLiabilities
-            # Iterate over keys and set values
-            Utilities.set_dictionary_xml(cl, current_liabilities)
-            # Parse other
-            l.StockholderEquity = liabilities.find('StockholderEquity').text
-
+                # Parse the liabilities
+                liabilities = balance.find('Liabilities')
+                l = r.Balance.Liabilities
+                # Parse the current assets
+                current_liabilities = liabilities.find('CurrentLiabilities')
+                cl = l.CurrentLiabilities
+                # Iterate over keys and set values
+                Utilities.set_dictionary_xml(cl, current_liabilities)
+                # Parse other
+                l.StockholderEquity = liabilities.find('StockholderEquity').text
 
             # Add the new annual report to the list
-            list.append(ar)
+            list.append(r)
+
+            # Filter the list
+            list = filter(function_filter, list)
